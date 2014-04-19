63639ad21b100fda9f867bc1b00f1f0e83fa2468
jcstewart09 <vagrant@kellogg-dev-box>
2014-04-19 01:07:34 +0000

    homework step 3 and attempt step 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Homework::Application.routes.draw do
 
 get "/dice/roll" => "dice/roll"
+get "/books/list" => "books/list"
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140418170840) do
+ActiveRecord::Schema.define(version: 20140418232002) do
 
-  create_table "dices", force: true do |t|
-    t.integer "roll"
+  create_table "books", force: true do |t|
+    t.string "title"
+    t.string "author"
   end
 
 end
